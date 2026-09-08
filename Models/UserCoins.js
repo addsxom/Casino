81ceea7ef32c24fa561afba15a3c0379d7c60ffb
!'Sx <userdeletshop@gmail.com>
2026-09-09 01:58:43 +0200

    rewards: persist message milestone progress

--- Models/UserCoins.js
+++ Models/UserCoins.js
@@ -25,43 +25,15 @@ const userCoinsSchema = new mongoose.Schema({
     type: Number,
     default: 0,
   },
+  messageRewardThreshold: {
+    type: Number,
+    default: 0,
+  },
 });
 
 userCoinsSchema.index(
   { userId: 1, guildId: 1 },
   { unique: true, name: 'user_coins_user_guild_unique' }
 );
-
-userCoinsSchema.methods.checkMessageThreshold = async function () {
-  const messageThresholds = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000];
-  const coinsPerThresholds = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000];
-
-  for (let i = 0; i < messageThresholds.length; i++) {
-    const messageThreshold = messageThresholds[i];
-    const coinsPerThreshold = coinsPerThresholds[i];
-
-    if (this.messages % messageThreshold === 0) {
-      const updatedAccount = await this.constructor.findOneAndUpdate(
-        { _id: this._id },
-        { $inc: { bank: coinsPerThreshold } },
-        { new: true }
-      );
-
-      if (!updatedAccount) {
-        return null;
-      }
-
-      this.bank = updatedAccount.bank;
-      this.coins = updatedAccount.coins;
-
-      return {
-        coins: coinsPerThreshold,
-        threshold: messageThreshold,
-      };
-    }
-  }
-
-  return null;
-};
 
 module.exports = mongoose.model('UserCoins', userCoinsSchema);
