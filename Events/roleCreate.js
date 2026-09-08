f26ce443986fe7ed19bad8f1a4a4d0b54693cf90
!'Sx <userdeletshop@gmail.com>
2026-09-08 14:59:42 +0200

    feat: add roleCreate staff log

---
+++ Events/roleCreate.js
@@ -0,0 +1,16 @@
+const { sendStaffLog, buildDiscordLog } = require('../utils/staffLogs.js');
+
+module.exports = async (_bot, role) => {
+  await sendStaffLog(
+    role.guild,
+    'discord-logs',
+    buildDiscordLog({
+      title: '➕ Rôle créé',
+      description: `${role} **${role.name}**`,
+      color: 0x57f287,
+      fields: [
+        { name: 'ID', value: `\`${role.id}\``, inline: true }
+      ]
+    })
+  );
+};
