0982fe1792da374acbed6d1af37ffcc13fa7d6d2
!'Sx <userdeletshop@gmail.com>
2026-09-08 14:58:14 +0200

    feat: add withdraw staff logs

--- Commands/Gestion coins/retall.js
+++ Commands/Gestion coins/retall.js
@@ -1,5 +1,6 @@
 const { EmbedBuilder } = require("discord.js");
 const { formatAmount } = require('../../utils/formatAmount.js');
+const { sendStaffLog, buildEconomyLog } = require('../../utils/staffLogs.js');
 const UserCoins = require('../../Models/UserCoins.js');
 
 module.exports = {
@@ -20,6 +21,20 @@ module.exports = {
       userCoins.coins += amountToWithdraw;
       userCoins.bank = 0;
       await userCoins.save();
+
+      await sendStaffLog(
+        message.guild,
+        'withdraw-logs',
+        buildEconomyLog({
+          title: '📤 Retrait total de la banque',
+          user: message.author,
+          amount: amountToWithdraw,
+          pocket: userCoins.coins,
+          bank: userCoins.bank,
+          sourceChannel: message.channel,
+          color: 0xfee75c
+        })
+      );
 
       return message.reply(`🏦・Vous avez retiré **${formatAmount(amountToWithdraw)}** de votre banque.`)
 
