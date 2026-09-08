109b44e57c2cfb874f875d8fbd90be9ece99c3fc
!'Sx <userdeletshop@gmail.com>
2026-09-08 19:26:34 +0200

    ui: simplify discord log embed

--- Events/channelDelete.js
+++ Events/channelDelete.js
@@ -8,12 +8,8 @@ module.exports = async (_bot, channel) => {
     'discord-logs',
     buildDiscordLog({
       title: '➖ Salon supprimé',
-      description: `**${channel.name}**`,
-      color: 0xed4245,
-      fields: [
-        { name: 'ID', value: `\`${channel.id}\``, inline: true },
-        { name: 'Type', value: `${channel.type}`, inline: true }
-      ]
+      description: `**#${channel.name}**`,
+      color: 0xed4245
     })
   );
 };
