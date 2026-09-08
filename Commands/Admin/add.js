1e01b91c883b58e6d32ee01abc0035289fc98ddb
!'Sx <userdeletshop@gmail.com>
2026-09-09 00:05:40 +0200

    economy: make admin add atomic

--- Commands/Admin/add.js
+++ Commands/Admin/add.js
@@ -1,9 +1,9 @@
-const UserCoins = require('../../Models/UserCoins.js');
 const parseAmount = require('../../utils/parseAmount.js');
 const { formatAmount } = require('../../utils/formatAmount.js');
 const { sendStaffLog, buildCoinMovementLog } = require('../../utils/staffLogs.js');
 
 const { requireBotOwner } = require('../../utils/ownerPermissions.js');
+const { creditBalance, incrementAccountField } = require('../../utils/economyService.js');
 
 module.exports = {
   name: 'add',
@@ -24,64 +24,65 @@ if (args.length !== 3) {
       return message.reply('Veuillez mentionner un utilisateur.');
     }
 
-    if (isNaN(amount) || amount <= 0) {
+    if (!Number.isSafeInteger(amount) || amount <= 0) {
       return message.reply('Veuillez spécifier un nombre valide supérieur à zéro.');
     }
 
     try {
-      let userCoins = await UserCoins.findOne({ userId: targetUser.id, guildId: message.guild.id });
-
-      if (!userCoins) {
-        userCoins = await UserCoins.create({ userId: targetUser.id, guildId: message.guild.id });
-      }
+      let userCoins;
 
       if (type === 'rep') {
-        userCoins.rep += amount;
-        await userCoins.save();
-        return message.reply(`Vous avez ajouté ${formatAmount(amount)} points de réputation à ${targetUser.tag}.`);
-      } else if (type === 'bank') {
-        userCoins.bank += amount;
-        await userCoins.save();
+        userCoins = await incrementAccountField({
+          userId: targetUser.id,
+          guildId: message.guild.id,
+          field: 'rep',
+          amount
+        });
 
-        await sendStaffLog(
-          message.guild,
-          'economy-logs',
-          buildCoinMovementLog({
-            title: '🛡️ Ajout admin',
-            user: targetUser,
-            delta: amount,
-            pocket: userCoins.coins,
-            bank: userCoins.bank,
-            reason: 'Ajout admin dans la banque',
-            sourceChannel: message.channel,
-            actor: message.author
-          })
+        return message.reply(
+          'Vous avez ajouté ' + formatAmount(amount) +
+          ' points de réputation à ' + targetUser.tag + '.'
         );
-
-        return message.reply(`Vous avez ajouté ${formatAmount(amount)} coins en bank à ${targetUser.tag}.`);
-      } else if (type === 'coins') {
-        userCoins.coins += amount;
-        await userCoins.save();
-
-        await sendStaffLog(
-          message.guild,
-          'economy-logs',
-          buildCoinMovementLog({
-            title: '🛡️ Ajout admin',
-            user: targetUser,
-            delta: amount,
-            pocket: userCoins.coins,
-            bank: userCoins.bank,
-            reason: 'Ajout admin dans la poche',
-            sourceChannel: message.channel,
-            actor: message.author
-          })
-        );
-
-        return message.reply(`Vous avez ajouté ${formatAmount(amount)} coins à ${targetUser.tag}.`);
-      } else {
-        return message.reply('Type invalide. Veuillez spécifier "rep", "bank" ou "coins".');
       }
+
+      if (type !== 'bank' && type !== 'coins') {
+        return message.reply(
+          'Type invalide. Veuillez spécifier "rep", "bank" ou "coins".'
+        );
+      }
+
+      userCoins = await creditBalance({
+        userId: targetUser.id,
+        guildId: message.guild.id,
+        target: type,
+        amount
+      });
+
+      await sendStaffLog(
+        message.guild,
+        'economy-logs',
+        buildCoinMovementLog({
+          title: '🛡️ Ajout admin',
+          user: targetUser,
+          delta: amount,
+          pocket: userCoins.coins,
+          bank: userCoins.bank,
+          reason:
+            type === 'bank'
+              ? 'Ajout admin dans la banque'
+              : 'Ajout admin dans la poche',
+          sourceChannel: message.channel,
+          actor: message.author
+        })
+      );
+
+      return message.reply(
+        type === 'bank'
+          ? 'Vous avez ajouté ' + formatAmount(amount) +
+            ' coins en bank à ' + targetUser.tag + '.'
+          : 'Vous avez ajouté ' + formatAmount(amount) +
+            ' coins à ' + targetUser.tag + '.'
+      );
     } catch (error) {
       console.error(error);
       return message.reply('Une erreur s\'est produite lors de l\'ajout de points de réputation/coins.');
