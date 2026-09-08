899c2ff1a9db28f4b8d0dea04498a298a2030f1f
!'Sx <userdeletshop@gmail.com>
2026-09-09 00:01:18 +0200

    economy: make pay atomic

--- Commands/Gestion coins/pay.js
+++ Commands/Gestion coins/pay.js
@@ -1,7 +1,10 @@
-const UserCoins = require('../../Models/UserCoins.js');
 const parseAmount = require('../../utils/parseAmount.js');
 const { formatAmount } = require('../../utils/formatAmount.js');
 const { sendStaffLog, buildTransferLog } = require('../../utils/staffLogs.js');
+const {
+  InsufficientFundsError,
+  transferCoins
+} = require('../../utils/economyService.js');
 
 module.exports = {
   name: 'pay',
@@ -29,61 +32,33 @@ module.exports = {
     const recipient = message.mentions.users.first()
       || message.client.users.cache.get(recipientArg);
 
+    if (!Number.isSafeInteger(amount) || amount <= 0) {
+      return message.reply(
+        'Veuillez fournir un montant valide à payer.\n' +
+        'Exemples : **+pay 2m @utilisateur** ou **+pay bank 2m @utilisateur**'
+      );
+    }
+
+    if (!recipient) {
+      return message.reply('Veuillez mentionner un utilisateur valide.');
+    }
+
+    if (recipient.bot) {
+      return message.reply('Tu ne peux pas envoyer de coins à un bot.');
+    }
+
+    if (recipient.id === senderId) {
+      return message.reply('Tu ne peux pas te payer toi-même.');
+    }
+
     try {
-      if (isNaN(amount) || amount <= 0) {
-        return message.reply(
-          'Veuillez fournir un montant valide à payer.\n' +
-          'Exemples : **+pay 2m @utilisateur** ou **+pay bank 2m @utilisateur**'
-        );
-      }
-
-      if (!recipient) {
-        return message.reply('Veuillez mentionner un utilisateur valide.');
-      }
-
-      if (recipient.bot) {
-        return message.reply('Tu ne peux pas envoyer de coins à un bot.');
-      }
-
-      if (recipient.id === senderId) {
-        return message.reply('Tu ne peux pas te payer toi-même.');
-      }
-
-      const senderCoins = await UserCoins.findOne({
-        userId: senderId,
-        guildId
+      const transfer = await transferCoins({
+        guildId,
+        senderId,
+        recipientId: recipient.id,
+        source,
+        amount
       });
-
-      if (!senderCoins || senderCoins[source] < amount) {
-        return message.reply(
-          source === 'bank'
-            ? '‼️・Tu n\'as pas assez de coins en banque.'
-            : '‼️・Tu n\'as pas assez de coins en poche.'
-        );
-      }
-
-      let recipientCoins = await UserCoins.findOne({
-        userId: recipient.id,
-        guildId
-      });
-
-      if (!recipientCoins) {
-        recipientCoins = await UserCoins.create({
-          userId: recipient.id,
-          guildId,
-          coins: 0,
-          bank: 0
-        });
-      }
-
-      const senderBefore = senderCoins[source];
-      const recipientBefore = recipientCoins.coins;
-
-      senderCoins[source] -= amount;
-      recipientCoins.coins += amount;
-
-      await senderCoins.save();
-      await recipientCoins.save();
 
       await sendStaffLog(
         message.guild,
@@ -93,10 +68,10 @@ module.exports = {
           recipient,
           amount,
           source,
-          senderBefore,
-          senderAfter: senderCoins[source],
-          recipientBefore,
-          recipientAfter: recipientCoins.coins
+          senderBefore: transfer.senderBefore,
+          senderAfter: transfer.senderAfter,
+          recipientBefore: transfer.recipientBefore,
+          recipientAfter: transfer.recipientAfter
         })
       );
 
@@ -104,7 +79,18 @@ module.exports = {
         `Tu as payé **${formatAmount(amount)}** coins💰 à ${recipient.tag} depuis ${source === 'bank' ? 'ta banque' : 'ta poche'}.`
       );
     } catch (error) {
-      console.error(error);
+      if (
+        error instanceof InsufficientFundsError ||
+        error?.code === 'INSUFFICIENT_FUNDS'
+      ) {
+        return message.reply(
+          source === 'bank'
+            ? '‼️・Tu n\'as pas assez de coins en banque.'
+            : '‼️・Tu n\'as pas assez de coins en poche.'
+        );
+      }
+
+      console.error('Pay transaction error:', error);
       return message.reply('Une erreur s\'est produite lors de la transaction.');
     }
   },
