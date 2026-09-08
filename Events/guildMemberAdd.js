0d04ce5ca74f6a7792d65b9e89364389f3ad1ccd
!'Sx <userdeletshop@gmail.com>
2026-09-08 19:26:15 +0200

    ui: simplify discord log embed

--- Events/guildMemberAdd.js
+++ Events/guildMemberAdd.js
@@ -8,18 +8,11 @@ module.exports = async (_bot, member) => {
     member.guild,
     'discord-logs',
     buildDiscordLog({
-      title: '📥 Membre arrivé',
-      description: `${member.user} **${member.user.tag}** a rejoint le serveur.`,
-      color: 0x57f287,
-      fields: [
-        { name: 'ID', value: `\`${member.id}\``, inline: true },
-        { name: 'Membres', value: `${member.guild.memberCount}`, inline: true },
-        {
-          name: 'Compte créé',
-          value: `<t:${Math.floor(member.user.createdTimestamp / 1000)}:R>`,
-          inline: true
-        }
-      ]
+      title: '📥 Arrivée',
+      description:
+        `${member.user} a rejoint le serveur.\n` +
+        `-# ${member.guild.memberCount} membres • Compte créé <t:${Math.floor(member.user.createdTimestamp / 1000)}:R>`,
+      color: 0x57f287
     })
   );
 };
