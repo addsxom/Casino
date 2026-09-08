8c17a762857a6c288243cdd1cfd987b333a0638e
!'Sx <userdeletshop@gmail.com>
2026-09-08 14:59:34 +0200

    feat: add channelCreate staff log

---
+++ Events/channelCreate.js
@@ -0,0 +1,19 @@
+const { sendStaffLog, buildDiscordLog } = require('../utils/staffLogs.js');
+
+module.exports = async (_bot, channel) => {
+  if (!channel.guild) return;
+
+  await sendStaffLog(
+    channel.guild,
+    'discord-logs',
+    buildDiscordLog({
+      title: '➕ Salon créé',
+      description: `${channel} **${channel.name}**`,
+      color: 0x57f287,
+      fields: [
+        { name: 'ID', value: `\`${channel.id}\``, inline: true },
+        { name: 'Type', value: `${channel.type}`, inline: true }
+      ]
+    })
+  );
+};
