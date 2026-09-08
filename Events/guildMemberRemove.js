edb7764a4acfd80945923cf1b392dde350ddd305
!'Sx <userdeletshop@gmail.com>
2026-09-08 14:59:23 +0200

    feat: log member leaves

--- Events/guildMemberRemove.js
+++ Events/guildMemberRemove.js
@@ -1,5 +1,20 @@
 const { updateMemberCount } = require('../utils/updateMemberCount.js');
+const { sendStaffLog, buildDiscordLog } = require('../utils/staffLogs.js');
 
 module.exports = async (_bot, member) => {
   await updateMemberCount(member.guild);
+
+  await sendStaffLog(
+    member.guild,
+    'discord-logs',
+    buildDiscordLog({
+      title: '📤 Membre parti',
+      description: `**${member.user.tag}** a quitté le serveur.`,
+      color: 0xed4245,
+      fields: [
+        { name: 'ID', value: `\`${member.id}\``, inline: true },
+        { name: 'Membres', value: `${member.guild.memberCount}`, inline: true }
+      ]
+    })
+  );
 };
