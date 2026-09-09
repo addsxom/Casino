639725bfead4ac1b87d3a52fa488613b085015af
!'Sx <userdeletshop@gmail.com>
2026-09-10 01:58:45 +0200

    slots: avoid duplicate megapot index declaration

--- Models/SlotMegaPot.js
+++ Models/SlotMegaPot.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const slotMegaPotSchema = new mongoose.Schema({
   guildId: {
     type: String,
-    required: true,
-    unique: true
+    required: true
   },
   amount: {
     type: Number,
