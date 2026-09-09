ba0be8466d9a6bb46a2444cbb4d08bfcd7b4db2a
!'Sx <userdeletshop@gmail.com>
2026-09-09 02:46:19 +0200

    rewards: persist voice reward progress model

---
+++ Models/VoiceRewardProgress.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const voiceRewardProgressSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true
+  },
+  guildId: {
+    type: String,
+    required: true
+  },
+  validMs: {
+    type: Number,
+    default: 0
+  },
+  targetMs: {
+    type: Number,
+    required: true
+  },
+  mutedMs: {
+    type: Number,
+    default: 0
+  },
+  previousSelfMute: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true
+});
+
+voiceRewardProgressSchema.index(
+  { userId: 1, guildId: 1 },
+  {
+    unique: true,
+    name: 'voice_reward_progress_user_guild_unique'
+  }
+);
+
+module.exports = mongoose.model(
+  'VoiceRewardProgress',
+  voiceRewardProgressSchema
+);
