56fbe6ab0de973be605f3ecb6aa0803d1fac86a2
!'Sx <userdeletshop@gmail.com>
2026-09-10 00:17:20 +0200

    rewards: persist afk farm progress

---
+++ Models/AfkRewardProgress.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const afkRewardProgressSchema =
+  new mongoose.Schema({
+    userId: {
+      type: String,
+      required: true
+    },
+    guildId: {
+      type: String,
+      required: true
+    },
+    validMs: {
+      type: Number,
+      default: 0
+    },
+    targetMs: {
+      type: Number,
+      required: true
+    }
+  }, {
+    timestamps: true
+  });
+
+afkRewardProgressSchema.index(
+  { userId: 1, guildId: 1 },
+  {
+    unique: true,
+    name: 'afk_reward_progress_user_guild_unique'
+  }
+);
+
+module.exports = mongoose.model(
+  'AfkRewardProgress',
+  afkRewardProgressSchema
+);
