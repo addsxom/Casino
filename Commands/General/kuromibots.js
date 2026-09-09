02da61acc18848f1a328dfbcee5407a72614772c
!'Sx <userdeletshop@gmail.com>
2026-09-10 00:56:14 +0200

    ui: embed kuromibots responses

--- Commands/General/kuromibots.js
+++ Commands/General/kuromibots.js
@@ -1,12 +1,20 @@
 const Discord = require("discord.js");
 
+const { replyEmbedPayload } = require('../../utils/replyEmbed.js');
+
 module.exports = {
   name: "kuromibots",
   description: "Donne une invitation pour le serveur de support Kuromi.",
   async execute(message, args) {
 
-    const supportMessage = '**<a:b_kuromi5:1176626430877106216>・Support Bots : https://discord.gg/kuromibots **'
-
-    message.channel.send(supportMessage);
+    return message.channel.send(
+      replyEmbedPayload(
+        '<a:b_kuromi5:1176626430877106216> **Support Bots**\nhttps://discord.gg/kuromibots',
+        {
+          type: 'info',
+          title: '💬 Serveur de support'
+        }
+      )
+    );
   },
 };
